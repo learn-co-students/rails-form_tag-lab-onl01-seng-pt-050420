7cc4b2c5c90a0910aa71d2d972cf613617659478
Catheryn Small <catheryn.small@gmail.com>
2020-09-10 23:33:09 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,15 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+  end
+
   def show
     @student = Student.find(params[:id])
   end
 
+  def create
+    Student.create(params.require(:student).permit("first_name", "last_name"))
+    redirect_to students_path
+  end
 end
